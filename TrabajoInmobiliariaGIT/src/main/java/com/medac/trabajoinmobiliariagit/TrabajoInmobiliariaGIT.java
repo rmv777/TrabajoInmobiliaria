79ef7d58fb32eed8b4c943c53574668238161c9f
Raúl Muñoz <128885714+rmv777@users.noreply.github.com>
2023-12-01 08:31:43 +0100

    Carga a GitHub

---
+++ TrabajoInmobiliariaGIT/src/main/java/com/medac/trabajoinmobiliariagit/TrabajoInmobiliariaGIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.medac.trabajoinmobiliariagit;
+
+/**
+ *
+ * @author rmv7
+ */
+public class TrabajoInmobiliariaGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
